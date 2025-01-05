70d23b4fe5e014f9638a07c408808191edd3baf4
GarlicRot <104709774+GarlicRot@users.noreply.github.com>
2025-01-05 09:16:46 -0800

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -12,27 +12,27 @@ if (!fs.existsSync(parsedBadgesPath)) {
 const parsedBadges = JSON.parse(fs.readFileSync(parsedBadgesPath, 'utf8'));
 let readmeContent = fs.readFileSync(readmePath, 'utf8');
 
-let changesMade = false;
+// Generate new plugins list
+const newPluginsList = parsedBadges
+  .map(
+    (plugin) =>
+      `- [${plugin.name}](https://github.com/${plugin.repo}) [![GitHub Downloads (all releases)](https://img.shields.io/github/downloads/${plugin.repo}/total)](${plugin.releaseUrl})`
+  )
+  .join('\n');
 
-// Loop through each plugin and update README if necessary
-parsedBadges.forEach(plugin => {
-  const downloadBadgeRegex = new RegExp(
-    `\\!\GitHub Downloads \all releases\\\https://img\\.shields\\.io/github/downloads/${plugin.repo}/total\.*?\`,
-    'g'
-  );
+// Define regex to match the plugins list section
+const pluginsListRegex = /<!-- START PLUGINS LIST -->([\s\S]*?)<!-- END PLUGINS LIST -->/;
 
-  const newDownloadBadge = `[![GitHub Downloads (all releases)](https://img.shields.io/github/downloads/${plugin.repo}/total)](${plugin.releaseUrl})`;
+// Replace the old plugins list with the new one
+const updatedReadmeContent = readmeContent.replace(
+  pluginsListRegex,
+  `<!-- START PLUGINS LIST -->\n${newPluginsList}\n<!-- END PLUGINS LIST -->`
+);
 
-  if (readmeContent.match(downloadBadgeRegex)) {
-    readmeContent = readmeContent.replace(downloadBadgeRegex, newDownloadBadge);
-    changesMade = true;
-  }
-});
-
-// Save updated README if changes were made
-if (changesMade) {
-  fs.writeFileSync(readmePath, readmeContent);
-  console.log('README updated with latest download URLs.');
+// Check if changes were made
+if (updatedReadmeContent !== readmeContent) {
+  fs.writeFileSync(readmePath, updatedReadmeContent);
+  console.log('README updated with latest plugin list.');
   process.exit(0); // Indicate changes were made
 } else {
   console.log('No changes made to README.');
