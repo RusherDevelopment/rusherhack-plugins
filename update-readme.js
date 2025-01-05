3e3f02550a14b5eccb9b87f2673f2ad536fc360d
GarlicRot <104709774+GarlicRot@users.noreply.github.com>
2025-01-05 09:46:28 -0800

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -12,31 +12,45 @@ if (!fs.existsSync(parsedBadgesPath)) {
 const parsedBadges = JSON.parse(fs.readFileSync(parsedBadgesPath, 'utf8'));
 let readmeContent = fs.readFileSync(readmePath, 'utf8');
 
-// Generate new plugins list
-const newPluginsList = parsedBadges
-  .map(
-    (plugin) =>
-      `- [${plugin.name}](https://github.com/${plugin.repo}) [![GitHub Downloads (all releases)](https://img.shields.io/github/downloads/${plugin.repo}/total)](${plugin.releaseUrl})`
-  )
-  .join('\n');
-
 // Define regex to match the plugins list section
 const pluginsListRegex = /<!-- START PLUGINS LIST -->([\s\S]*?)<!-- END PLUGINS LIST -->/;
+const existingPluginsMatch = readmeContent.match(pluginsListRegex);
 
-// Replace the old plugins list with the new one
+if (!existingPluginsMatch) {
+  console.error('Error: Could not find plugins list section in README.');
+  process.exit(1);
+}
+
+let existingPluginsList = existingPluginsMatch[1];
+let changesMade = false;
+
+// Update download URLs if they differ
+parsedBadges.forEach((plugin) => {
+  const pluginRegex = new RegExp(
+    `- \${plugin.name}\\https://github\\.com/${plugin.repo}\ \!\GitHub Downloads \all releases\\\https://img\\.shields\\.io/github/downloads/${plugin.repo}/total\\\.*?\`,
+    'g'
+  );
+
+  const newPluginEntry = `- [${plugin.name}](https://github.com/${plugin.repo}) [![GitHub Downloads (all releases)](https://img.shields.io/github/downloads/${plugin.repo}/total)](${plugin.releaseUrl})`;
+
+  if (existingPluginsList.match(pluginRegex)) {
+    existingPluginsList = existingPluginsList.replace(pluginRegex, newPluginEntry);
+    changesMade = true;
+  }
+});
+
+// Replace the plugins list in the README
 const updatedReadmeContent = readmeContent.replace(
   pluginsListRegex,
-  `<!-- START PLUGINS LIST -->\n${newPluginsList}\n<!-- END PLUGINS LIST -->`
+  `<!-- START PLUGINS LIST -->\n${existingPluginsList}\n<!-- END PLUGINS LIST -->`
 );
 
 // Check if changes were made
-if (updatedReadmeContent !== readmeContent) {
+if (changesMade) {
   fs.writeFileSync(readmePath, updatedReadmeContent);
-  console.log('README updated with latest plugin list.');
+  console.log('README updated with latest download URLs.');
   process.exit(0); // Indicate changes were made
 } else {
   console.log('No changes made to README.');
   process.exit(1); // Indicate no changes were made
 }
-
-console.log('::set-output name=changes-found::true');
